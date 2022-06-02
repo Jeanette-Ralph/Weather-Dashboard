32fc637aebef4a715775ba936f5a4936fad610cd
Jeanette Ralph <jean.ralph@hotmail.com>
2022-06-02 14:34:48 -0700

    first commit

---
+++ Assets/js/script.js
@@ -0,0 +1,16 @@
+// globally scoped variables
+var searchBtn = document.getElementById('search-button');
+var cityBtn = document.getElementById('city-button');
+
+// event listeners for search button
+searchBtn.addEventListener('click',);
+
+// event listner for all the city buttons
+cityBtn.addEventListener('click',);
+
+// function for on click search button
+    // presented with current and future conditions
+    // that city is added to the search history
+
+
+
